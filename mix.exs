7466e6d602c9635eeac14582af2db3b109e819f6
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-10-19 14:52:30 -0300

    format and lint

--- mix.exs
+++ mix.exs
@@ -70,7 +70,10 @@ defmodule DoctorSchedule.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.8", only: :dev},
+      {:excoveralls, "~> 0.13.0", only: :test}
     ]
   end
 
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -16,6 +16,7 @@ defmodule DoctorScheduleWeb.ConnCase do
   """
 
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL.Sandbox
 
   using do
     quote do
@@ -32,10 +33,10 @@ defmodule DoctorScheduleWeb.ConnCase do
   end
 
   setup tags do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(DoctorSchedule.Repo)
+    :ok = Sandbox.checkout(DoctorSchedule.Repo)
 
     unless tags[:async] do
-      Ecto.Adapters.SQL.Sandbox.mode(DoctorSchedule.Repo, {:shared, self()})
+      Sandbox.mode(DoctorSchedule.Repo, {:shared, self()})
     end
 
     {:ok, conn: Phoenix.ConnTest.build_conn()}
