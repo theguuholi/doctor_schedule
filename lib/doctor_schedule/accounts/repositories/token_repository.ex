ba9030c619e2c3261f3f0c9cfa7cc85b6086df07
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-11-11 18:59:39 -0300

    format

--- lib/doctor_schedule/accounts/repositories/token_repository.ex
+++ lib/doctor_schedule/accounts/repositories/token_repository.ex
@@ -11,19 +11,20 @@ defmodule DoctorSchedule.Accounts.Repositories.TokenRepository do
 
   def get_by_token(token), do: Repo.get_by(UserToken, token: token) |> Repo.preload(:user)
 
-
   def generate(email) do
     Repo.get_by(User, email: email)
     |> case do
-      nil -> {:error, "User does not exists"}
+      nil ->
+        {:error, "User does not exists"}
+
       user ->
-        {:ok, user_token} = user
-        |> Ecto.build_assoc(:user_tokens)
-        |> UserToken.changeset()
-        |> Repo.insert()
+        {:ok, user_token} =
+          user
+          |> Ecto.build_assoc(:user_tokens)
+          |> UserToken.changeset()
+          |> Repo.insert()
+
         {:ok, user_token.token, user}
     end
   end
-
-
 end
