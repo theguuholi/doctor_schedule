b71f06d00c53314c60351e15af10d97f4002edf0
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-11-13 13:09:08 -0300

    increase coverage

--- lib/doctor_schedule_web/router.ex
+++ lib/doctor_schedule_web/router.ex
@@ -46,12 +46,13 @@ defmodule DoctorScheduleWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
+  # coveralls-ignore-start
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
-
     scope "/" do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: DoctorScheduleWeb.Telemetry
     end
   end
+  # coveralls-ignore-stop
 end
