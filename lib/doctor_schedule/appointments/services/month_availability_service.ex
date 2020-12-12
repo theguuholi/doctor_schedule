2e128536ccd831a80751e8ef9b99db3981e48406
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-12-12 19:44:53 -0300

    testes de disponibilidades

--- lib/doctor_schedule/appointments/services/month_availability_service.ex
+++ lib/doctor_schedule/appointments/services/month_availability_service.ex
@@ -8,11 +8,13 @@ defmodule DoctorSchedule.Appointments.Services.MonthAvailabilityService do
     today = Date.utc_today()
 
     1..Date.days_in_month(this_month)
-    |> Enum.map(&%{day: &1, available: is_available?(&1, today, schedules_month)})
+    |> Enum.map(&%{day: &1, available: is_available?(&1, today, this_month, schedules_month)})
   end
 
-  def is_available?(day, today, schedules_month) do
-    if day >= today do
+  def is_available?(day, today, this_month, schedules_month) do
+    date = %Date{this_month | day: day}
+
+    if Date.compare(date, today) != :lt do
       schedules_month
       |> Enum.filter(&(&1.date.day == day))
       |> Enum.count() < 12
--- test/doctor_schedule/appointments/services/month_availability_service_test.exs
+++ test/doctor_schedule/appointments/services/month_availability_service_test.exs
@@ -6,23 +6,40 @@ defmodule DoctorSchedule.Appointments.Services.MonthAvailabilityServiceTest do
 
   test "it should see all available hour" do
     provider = UserFixture.create_provider()
-    date = Date.utc_today()
-    date = %Date{date | month: date.month - 2}
-    response = MonthAvailabilityService.execute(provider.id, date.year, date.month)
+    response = MonthAvailabilityService.execute(provider.id, 2019, 12)
 
     assert [
-             %{available: false, hour: 8},
-             %{available: false, hour: 9},
-             %{available: false, hour: 10},
-             %{available: false, hour: 11},
-             %{available: false, hour: 12},
-             %{available: false, hour: 13},
-             %{available: false, hour: 14},
-             %{available: false, hour: 15},
-             %{available: false, hour: 16},
-             %{available: false, hour: 17},
-             %{available: false, hour: 18},
-             %{available: false, hour: 19}
+             %{available: false, day: 1},
+             %{available: false, day: 2},
+             %{available: false, day: 3},
+             %{available: false, day: 4},
+             %{available: false, day: 5},
+             %{available: false, day: 6},
+             %{available: false, day: 7},
+             %{available: false, day: 8},
+             %{available: false, day: 9},
+             %{available: false, day: 10},
+             %{available: false, day: 11},
+             %{available: false, day: 12},
+             %{available: false, day: 13},
+             %{available: false, day: 14},
+             %{available: false, day: 15},
+             %{available: false, day: 16},
+             %{available: false, day: 17},
+             %{available: false, day: 18},
+             %{available: false, day: 19},
+             %{available: false, day: 20},
+             %{available: false, day: 21},
+             %{available: false, day: 22},
+             %{available: false, day: 23},
+             %{available: false, day: 24},
+             %{available: false, day: 25},
+             %{available: false, day: 26},
+             %{available: false, day: 27},
+             %{available: false, day: 28},
+             %{available: false, day: 29},
+             %{available: false, day: 30},
+             %{available: false, day: 31}
            ] == response
   end
 end
