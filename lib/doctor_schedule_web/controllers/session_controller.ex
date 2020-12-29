b30886d71b47355718d2d514b0e68b0d1b926a5a
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-12-29 07:54:55 -0300

    start style html

---
+++ lib/doctor_schedule_web/controllers/session_controller.ex
@@ -0,0 +1,8 @@
+defmodule DoctorScheduleWeb.SessionController do
+  use DoctorScheduleWeb, :controller
+
+  def session(conn, _params) do
+    render(conn, "session.html")
+  end
+
+end
--- lib/doctor_schedule_web/router.ex
+++ lib/doctor_schedule_web/router.ex
@@ -22,6 +22,7 @@ defmodule DoctorScheduleWeb.Router do
     pipe_through :browser
 
     live "/", PageLive, :index
+    get "/session", SessionController, :session
   end
 
   # Other scopes may use custom stacks.
