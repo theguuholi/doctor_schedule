4d93e51fc00690c7d317f3a94449fa98884c4116
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-03-30 11:25:04 -0300

    page live

--- lib/doctor_schedule_web/live/page_live.ex
+++ lib/doctor_schedule_web/live/page_live.ex
@@ -6,7 +6,7 @@ defmodule DoctorScheduleWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket |> new() |> insert_days}
+    {:ok, socket |> insert_days}
   end
 
   @impl true
@@ -14,45 +14,7 @@ defmodule DoctorScheduleWeb.PageLive do
     View.render(PageView, "index.html", assigns)
   end
 
-  defp new(socket), do: assign(socket, counters: Counters.new())
-
   defp insert_days(socket) do
     assign(socket, days: 1..31 |> Enum.map(& &1))
-  end
-
-  defp add(socket) do
-    assign(socket, counters: Counters.add_counter(socket.assigns.counters))
-  end
-
-  defp zer(socket, name) do
-    assign(socket, counters: Counters.zer(socket.assigns.counters, name))
-  end
-
-  defp dec(socket, name) do
-    assign(socket, counters: Counters.dec(socket.assigns.counters, name))
-  end
-
-  defp inc(socket, name) do
-    assign(socket, counters: Counters.inc(socket.assigns.counters, name))
-  end
-
-  @impl true
-  def handle_event("add", _params, socket) do
-    {:noreply, add(socket)}
-  end
-
-  @impl true
-  def handle_event("inc", %{"counter" => name}, socket) do
-    {:noreply, inc(socket, name)}
-  end
-
-  @impl true
-  def handle_event("dec", %{"counter" => name}, socket) do
-    {:noreply, dec(socket, name)}
-  end
-
-  @impl true
-  def handle_event("zer", %{"counter" => name}, socket) do
-    {:noreply, zer(socket, name)}
   end
 end
